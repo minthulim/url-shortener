d82348c100950f69e010c5fa96c86384b63d0e57
Min Thu Aung <reoknite@gmail.com>
2019-06-24 17:07:31 -0400

    config for CodeCov

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ const {defaults} = require('jest-config');
 module.exports = {
   testEnvironment: 'node',
   testPathIgnorePatterns: [...defaults.testPathIgnorePatterns, '/testUtils/'],
-  clearMocks: true, // clear info stored in mocks between tests
+  clearMocks: true, // clear info stored in mocks between tests,
+  // codeCov
+  collectCoverage: true,
+  coverageDirectory: './coverage/'
 };
